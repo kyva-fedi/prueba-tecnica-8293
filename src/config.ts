4b366c6d22e71ce79d16daa43724afa02193dd40
Kyva Iborra <hey@kyva.es>
2024-07-07 15:06:08 +0200

    change env name

--- src/config.ts
+++ src/config.ts
@@ -2,5 +2,5 @@ export const apiUrl = 'http://localhost:3001'
 
 export const youtube = {
   apiUrl: 'https://www.googleapis.com/youtube/v3',
-  authKey: import.meta.env.VITE_TEST_KEY
+  authKey: import.meta.env.VITE_TEST
 }
